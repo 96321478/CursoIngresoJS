86822dd8636e15834f31dfbd8c2549d1dde26c9c
96321478 <52535192+96321478@users.noreply.github.com>
2019-07-31 00:37:40 -0300

    Add files via upload

---
+++ jsInstruccionIF (10).js
@@ -0,0 +1,22 @@
+function mostrar()
+{		var nota= 5;
+
+
+		if ( nota >= 9) {
+			
+			alert("excelente");
+		
+				} else if (nota > 4) {
+
+					alert("Estas aprobado");
+			
+		
+						} else { alert("suerte para la próxima ");
+			
+		}
+
+
+	//Genero el número RANDOM entre 1 y 10 
+	
+
+}//FIN DE LA FUNCIÓN
