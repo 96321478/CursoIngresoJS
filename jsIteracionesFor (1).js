75b1d5878d06c8c6c49475556ee2991a859c334b
96321478 <52535192+96321478@users.noreply.github.com>
2019-07-31 10:41:17 -0300

    Add files via upload

---
+++ jsIteracionesFor (1).js
@@ -0,0 +1,28 @@
+function mostrar()
+{
+
+
+
+
+for (var cont = 1 ; cont < 11 ; cont ++) {
+
+    
+
+
+
+    document.write(cont) + "<br>";
+
+
+
+}
+
+}
+
+
+
+
+
+
+
+
+
---
+++ jsIteracionesFor (3).js
@@ -0,0 +1,19 @@
+function mostrar()
+{
+
+var cant; 
+
+     cant = prompt("ingrese el número de repeticiones");
+
+var i = 10;
+
+for (var i = cant ; i < 10 ; i--) {
+    
+    
+    console.log("Hola utnfra ");
+
+}
+
+
+
+}//FIN DE LA FUNCIÓN
