22cd9e622197770f22235ebfbbb067a3bb5c82ce
96321478 <52535192+96321478@users.noreply.github.com>
2019-08-01 12:13:00 -0300

    Add files via upload

---
+++ seisyo.js
@@ -0,0 +1,41 @@
+function mostrar()
+{
+
+    var hora = document.getElementById('laHora').value;
+
+    switch (hora) {
+        case     "6":
+        case     "7":
+        case     "8":
+        case     "9":
+        case    "10":
+        case    "11":
+             
+        alert("Es de mañana ");
+
+            break;
+        
+        case    "12":
+        case    "13":
+        case    "14":
+        case    "15":
+        case    "16":
+        case    "17":
+        case    "18":    
+        case    "19":
+
+        alert("Es de tarde ");
+
+        break;
+
+        default:
+
+
+        alert("Es de noche");
+
+
+            break;
+    }
+            
+
+}
